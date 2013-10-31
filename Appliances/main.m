9f8831c8060dbb65794b232d6c93bd6042d60e21
Arun Agrawal <arunagw@gmail.com>
2013-11-01 00:01:37 +0100

    Initial Commit

---
+++ Appliances/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Appliances
+//
+//  Created by Arun Agrawal on 01/11/13.
+//  Copyright (c) 2013 Arun Agrawal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
